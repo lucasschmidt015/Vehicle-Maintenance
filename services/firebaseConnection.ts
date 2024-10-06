9ebcf6eef40005d01561202cadfbddb42488fb9d
lucasschmidt015 <lucas.schmidt015@gmail.com>
2024-10-06 16:49:35 -0300

    Removed react-native-dotenv (incompatible with expo-router).

--- services/firebaseConnection.ts
+++ services/firebaseConnection.ts
@@ -1,16 +1,14 @@
-import { API_KEY, GCM_SENDER_ID, PROJECT_ID, STORAGE_BUCKET, GOOGLE_APP_ID } from '@env';
-
 import firebase from 'firebase/compat/app'; 
 import 'firebase/compat/auth'; 
 import 'firebase/compat/firestore'; 
 
 const firebaseConfig = {
-  apiKey: API_KEY,
-  authDomain: `${PROJECT_ID}.firebaseapp.com`,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: GCM_SENDER_ID,
-  appId: GOOGLE_APP_ID,
+  apiKey: process.env.EXPO_PUBLIC_API_KEY,
+  authDomain: `${process.env.EXPO_PUBLIC_PROJECT_ID}.firebaseapp.com`,
+  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_GCM_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_GOOGLE_APP_ID,
 };
 
 if (!firebase.apps.length) {
